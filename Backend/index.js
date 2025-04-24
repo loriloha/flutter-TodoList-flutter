01a8212c580600701cf2030ec2c47b67c2667ba5
loriloha <loriloha31@gmail.com>
2025-04-24 04:50:27 +0300

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -4,5 +4,5 @@ const db = require('./config/db');
 const port =3000;
 
 app.listen(port, () => {
-    console.log(`Server Listening on Port http://0.0.0.0:${port}`);
+    console.log(`Server Listening on Port https://flutter-todolist-assignment.onrender.com:${port}`);
 });
